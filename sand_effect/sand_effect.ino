969ef73728477794466d215ade436a6f71c1d306
pranaytummalapalli <31589727+pranaytummalapalli@users.noreply.github.com>
2018-04-23 13:35:16 +0530

    Update sand_effect.ino

--- sand_effect/sand_effect.ino
+++ sand_effect/sand_effect.ino
@@ -1,161 +1,161 @@
-#define l1 (2)
-#define l2 (3)
-#define l3 (4)
-#define l4 (5)
-#define l5 (6)
-#define l6 (7)
+int L1 = 2;
+int L2 = 3;
+int L3 = 4;
+int L4 = 5;
+int L5 = 6;
+int L6 = 7;
 
 void setup()
 {
-  pinMode(l1, OUTPUT);
-  pinMode(l2, OUTPUT);
-  pinMode(l3, OUTPUT);
-  pinMode(l4, OUTPUT);
-  pinMode(l5, OUTPUT);
-  pinMode(l6, OUTPUT);
+  pinMode(L1, OUTPUT);
+  pinMode(L2, OUTPUT);
+  pinMode(L3, OUTPUT);
+  pinMode(L4, OUTPUT);
+  pinMode(L5, OUTPUT);
+  pinMode(L6, OUTPUT);
 }
 
 void loop1()
 {
-  digitalWrite(l1, HIGH);  
+  digitalWrite(L1, HIGH);  
   delay(100);
-  digitalWrite(l1, LOW);
+  digitalWrite(L1, LOW);
   
-  digitalWrite(l2, HIGH);
+  digitalWrite(L2, HIGH);
   delay(100);
-  digitalWrite(l2, LOW);
+  digitalWrite(L2, LOW);
   
-  digitalWrite(l3, HIGH);
+  digitalWrite(L3, HIGH);
   delay(100);
-  digitalWrite(l3, LOW);
+  digitalWrite(L3, LOW);
  
-  digitalWrite(l4, HIGH);
+  digitalWrite(L4, HIGH);
   delay(100);
-  digitalWrite(l4, LOW);
+  digitalWrite(L4, LOW);
   
-  digitalWrite(l5, HIGH);
+  digitalWrite(L5, HIGH);
   delay(100);
-  digitalWrite(l5, LOW);
+  digitalWrite(L5, LOW);
   
-  digitalWrite(l6, HIGH);
+  digitalWrite(L6, HIGH);
   delay(100);
-  digitalWrite(l6, LOW);
+  digitalWrite(L6, LOW);
   
 }
 
 void loop2()
 {
-  digitalWrite(l6, HIGH);
+  digitalWrite(L6, HIGH);
   
-  digitalWrite(l1, HIGH);  
+  digitalWrite(L1, HIGH);  
   delay(100);
-  digitalWrite(l1, LOW);
+  digitalWrite(L1, LOW);
   
-  digitalWrite(l2, HIGH);
+  digitalWrite(L2, HIGH);
   delay(100);
-  digitalWrite(l2, LOW);
+  digitalWrite(L2, LOW);
   
-  digitalWrite(l3, HIGH);
+  digitalWrite(L3, HIGH);
   delay(100);
-  digitalWrite(l3, LOW);
+  digitalWrite(L3, LOW);
  
-  digitalWrite(l4, HIGH);
+  digitalWrite(L4, HIGH);
   delay(100);
-  digitalWrite(l4, LOW);
+  digitalWrite(L4, LOW);
   
-  digitalWrite(l5, HIGH);
+  digitalWrite(L5, HIGH);
   delay(100);
-  digitalWrite(l5, LOW);
+  digitalWrite(L5, LOW);
 }
 
 void loop3()
 {
-   digitalWrite(l6, HIGH);
-   digitalWrite(l5, HIGH);
+   digitalWrite(L6, HIGH);
+   digitalWrite(L5, HIGH);
    
-   digitalWrite(l1, HIGH);  
+   digitalWrite(L1, HIGH);  
   delay(100);
-  digitalWrite(l1, LOW);
+  digitalWrite(L1, LOW);
   
-  digitalWrite(l2, HIGH);
+  digitalWrite(L2, HIGH);
   delay(100);
-  digitalWrite(l2, LOW);
+  digitalWrite(L2, LOW);
   
-  digitalWrite(l3, HIGH);
+  digitalWrite(L3, HIGH);
   delay(100);
-  digitalWrite(l3, LOW);
+  digitalWrite(L3, LOW);
  
-  digitalWrite(l4, HIGH);
+  digitalWrite(L4, HIGH);
   delay(100);
-  digitalWrite(l4, LOW);
+  digitalWrite(L4, LOW);
 }
 
 void loop4()
 {
-  digitalWrite(l6, HIGH);
-   digitalWrite(l5, HIGH);
-   digitalWrite(l4, HIGH);
+  digitalWrite(L6, HIGH);
+  digitalWrite(L5, HIGH);
+  digitalWrite(L4, HIGH);
    
-   digitalWrite(l1, HIGH);  
+  digitalWrite(L1, HIGH);  
   delay(100);
-  digitalWrite(l1, LOW);
+  digitalWrite(L1, LOW);
   
-  digitalWrite(l2, HIGH);
+  digitalWrite(L2, HIGH);
   delay(100);
-  digitalWrite(l2, LOW);
+  digitalWrite(L2, LOW);
   
-  digitalWrite(l3, HIGH);
+  digitalWrite(L3, HIGH);
   delay(100);
-  digitalWrite(l3, LOW);
+  digitalWrite(L3, LOW);
  }
 
  void loop5()
  {
-  digitalWrite(l6, HIGH);
-   digitalWrite(l5, HIGH);
-   digitalWrite(l4, HIGH);
-   digitalWrite(l3, HIGH);
+  digitalWrite(L6, HIGH);
+  digitalWrite(L5, HIGH);
+  digitalWrite(L4, HIGH);
+  digitalWrite(L3, HIGH);
    
-   digitalWrite(l1, HIGH);  
+  digitalWrite(L1, HIGH);  
   delay(100);
-  digitalWrite(l1, LOW);
+  digitalWrite(L1, LOW);
   
-  digitalWrite(l2, HIGH);
+  digitalWrite(L2, HIGH);
   delay(100);
-  digitalWrite(l2, LOW);
+  digitalWrite(L2, LOW);
  }
 
  void loop6()
  {
-  digitalWrite(l6, HIGH);
-   digitalWrite(l5, HIGH);
-   digitalWrite(l4, HIGH);
-   digitalWrite(l3, HIGH);
-   digitalWrite(l2, HIGH);
+  digitalWrite(L6, HIGH);
+  digitalWrite(L5, HIGH);
+  digitalWrite(L4, HIGH);
+  digitalWrite(L3, HIGH);
+  digitalWrite(L2, HIGH);
    
-   digitalWrite(l1, HIGH);  
+  digitalWrite(L1, HIGH);  
   delay(100);
-  digitalWrite(l1, LOW);
+  digitalWrite(L1, LOW);
  }
 
  void loop7()
  {
-  digitalWrite(l6, HIGH);
-   digitalWrite(l5, HIGH);
-   digitalWrite(l4, HIGH);
-   digitalWrite(l3, HIGH);
-   digitalWrite(l2, HIGH);
-   digitalWrite(l1, HIGH);
+  digitalWrite(L6, HIGH);
+  digitalWrite(L5, HIGH);
+  digitalWrite(L4, HIGH);
+  digitalWrite(L3, HIGH);
+  digitalWrite(L2, HIGH);
+  digitalWrite(L1, HIGH);
  }
 
  void loop8()
  {
-  digitalWrite(l6, LOW);
-   digitalWrite(l5, LOW);
-   digitalWrite(l4, LOW);
-   digitalWrite(l3, LOW);
-   digitalWrite(l2, LOW);
-   digitalWrite(l1, LOW);
+  digitalWrite(L6, LOW);
+  digitalWrite(L5, LOW);
+  digitalWrite(L4, LOW);
+  digitalWrite(L3, LOW);
+  digitalWrite(L2, LOW);
+  digitalWrite(L1, LOW);
  }
 
 
